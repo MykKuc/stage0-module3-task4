59c4a6102fa70ec56b17a55075eb8cdab7fd2eee
Mykoliux <mykolas.kucinskas2@gmail.com>
2022-07-16 15:01:02 +0300

    All tasks completed.

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char char1 = 99;
+        char char2 = 104;
+        char char3 = 97;
+        char char4 = 114;
     }
 }
